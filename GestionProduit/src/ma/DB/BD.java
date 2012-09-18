9d32f41d98b22f6302c818af08b402603021beec
Administrateur <Administrateur@PC_FORM>
2012-09-18 09:42:39 +0100

    ADIL OKBANI NAME

--- GestionProduit/src/ma/DB/BD.java
+++ GestionProduit/src/ma/DB/BD.java
@@ -12,7 +12,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import ma.controller.Client_controller;
 //import ma.view.Fichier;
-
+//ADIL OKBANI
 /**
  *
  * @author Administrateur
